9f0879dc2ac328142cc41533946db759d0d4caca
Trashcan411 <fahmiibrahim21193@gmail.com>
2025-03-21 00:25:00 +0700

    add css, add generating button unusable

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,15 +1,31 @@
-const container = document.querySelector("#container")
 
-for (i = 0; i< 16; i++){
+/*btn.addEventListener("click", function (e) {
+  e.target.style.background = "red";
+  });*/
+
+const buttonPrompt = document.querySelector("button")
+buttonPrompt.textContent = `make a new grid`;
+buttonPrompt.addEventListener("click", function(e){
+    let a = prompt("grid number?")
+    return a
+})
+
+const container = document.querySelector("#container")
+container.classList.add("container");
+
+for (i = 0; i< buttonPrompt; i++){
     const div = document.createElement("div");
-    div.classList.add = "div";
-    div.textContent = `${i}`;
+    div.classList.add("div");
+    div.style.display = "flex; flex-direction:row;"
     container.appendChild(div);
     
     for (b = 0; b< 16; b ++){
         const divc = document.createElement("div");
-        divc.classList.add = "divc"
-        divc.textContent = `${b}`
+        divc.classList.add("divc");
+        divc.textContent = `${b}`;
+        div.addEventListener("mouseover", function(e){
+            e.target.style.background= "black"
+        })
         div.appendChild(divc);
 
     }
