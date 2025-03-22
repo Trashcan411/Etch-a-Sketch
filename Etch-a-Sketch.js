10c906237459fa58b6ed38bce0a21ee2d9b788ee
Trashcan411 <fahmiibrahim21193@gmail.com>
2025-03-23 00:07:25 +0700

    add new button prompt grid number and its function

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -2,31 +2,48 @@
 /*btn.addEventListener("click", function (e) {
   e.target.style.background = "red";
   });*/
+const container = document.querySelector("#container")
+container.classList.add("container");
 
 const buttonPrompt = document.querySelector("button")
 buttonPrompt.textContent = `make a new grid`;
 buttonPrompt.addEventListener("click", function(e){
-    let a = prompt("grid number?")
-    return a
-})
+    if (container.hasChildNodes()){
+        let children = container.childNodes;
 
-const container = document.querySelector("#container")
-container.classList.add("container");
-
-for (i = 0; i< buttonPrompt; i++){
-    const div = document.createElement("div");
-    div.classList.add("div");
-    div.style.display = "flex; flex-direction:row;"
-    container.appendChild(div);
-    
-    for (b = 0; b< 16; b ++){
-        const divc = document.createElement("div");
-        divc.classList.add("divc");
-        divc.textContent = `${b}`;
-        div.addEventListener("mouseover", function(e){
-            e.target.style.background= "black"
-        })
-        div.appendChild(divc);
-
+        while (container.firstChild){
+            container.removeChild(container.firstChild)
+        }
     }
+    let a = parseInt(prompt("grid number?"))
+
+    while (a > 101){
+        a = parseInt(prompt("grid number?"))
+    }
+    
+    for (i = 0; i< a; i++){
+        const div = document.createElement("div");
+        div.classList.add("div");
+        div.style.display = "flex; flex-direction:row;"
+        container.appendChild(div);
+        for (b = 0; b< a; b ++){
+            const divc = document.createElement("div");
+            divc.classList.add("divc");
+            divc.addEventListener("mouseover", mouseOver);
+            divc.addEventListener("mouseout", mouseOut);
+            function mouseOver() {
+                divc.style.backgroundColor = "black";
+              }
+              
+              function mouseOut() {
+                divc.style.backgroundColor = "white";
+              }
+            div.appendChild(divc);
+            }
+
 }
+}
+)
+
+
+
